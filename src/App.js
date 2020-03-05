e7061789e6ca05f06a49f80d6a0b9e267e002640
giovanni1129 <giovanni.u@hotmail.com>
2020-03-05 10:21:44 -0600

    updated prop-it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,100 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import PersonCardComponents from './components/PersonCardComponents';
 
-function App() {
-  return (
-    <div className="App">
-      <PersonCardComponents firstName="Isaac" lastname="Vega" Age={25} hairColor="black"/>
-      <PersonCardComponents firstName="Vincent" lastname="Pulsen" Age={25} hairColor="brown"/>
-      <PersonCardComponents firstName="Carlos" lastname="Vela" Age={30} hairColor="pink"/>
-      <PersonCardComponents firstName="Christian" lastname="Pulisic" Age={20} hairColor="red"/>
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      persons : [
+        {
+          firstName: "Paul",
+          lastName: "Dybala",
+          age: 24,
+          hairColor: "Black"
+        },
+        {
+          firstName: "Eden",
+          lastName: "Hazard",
+          age: 27,
+          hairColor: "Brown"
+        }
+      ]
+    }
+    this.handelAgeClick = this.handelAgeClick.bind(this);
+  }
+
+  handelAgeClick(e,idx){
+    e.preventDefault();
+    this.setState(prevState => {
+      return {
+        age: prevState.persons[idx].age ++
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.persons.map((p,idx) => {
+            return <PersonCardComponents 
+                    firstName={p.firstName}
+                    lastName={p.lastName}
+                    age={p.age}
+                    hairColor={p.hairColor}
+                    key={idx}
+                    idx={idx}
+                    handleClick={this.handelAgeClick}
+                    />
+          })
+        }
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <PersonCardComponents firstName="Isaac" lastname="Vega" Age={25} hairColor="black"/>
+//       <PersonCardComponents firstName="Vincent" lastname="Pulsen" Age={25} hairColor="brown"/>
+//       <PersonCardComponents firstName="Carlos" lastname="Vela" Age={30} hairColor="pink"/>
+//       <PersonCardComponents firstName="Christian" lastname="Pulisic" Age={20} hairColor="red"/>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/PersonCardComponents.js
+++ src/components/PersonCardComponents.js
@@ -1,15 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-class PersonCardComponents extends Component {
-    render(){
-        return(
-            <div>
-                <h2>{this.props.firstName},{this.props.lastname}</h2>
-                <p>Age:{this.props.Age}</p>
-                <p>Hair Color:{this.props.hairColor}</p>
-            </div>
-        );
+const PersonCardComponents = ({firstName, lastName,age,hairColor,idx,handleClick}) => {
+    const style={
+        border: "1px solid black"
     }
+    return (
+        <div style={style} className="card col-5 mx-auto">
+            <h2>{lastName}, {firstName}</h2>
+            <p>Age: {age}</p>
+            <p>hairColor: {hairColor}</p>
+            <button className="btn btn-outline-primary btn-dark col-2 mx-auto"onClick={(e) => handleClick(e,idx)}>Birthday</button>
+        </div>
+    ); 
 }
+
 export default PersonCardComponents;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class PersonCardComponents extends Component {
+//     render(){
+//         return(
+//             <div>
+//                 <h2>{this.props.firstName},{this.props.lastname}</h2>
+//                 <p>Age:{this.props.Age}</p>
+//                 <p>Hair Color:{this.props.hairColor}</p>
+//             </div>
+//         );
+//     }
+// }
+// export default PersonCardComponents;
